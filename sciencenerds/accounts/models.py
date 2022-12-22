e8e98ff47f5545654483313c569999e0a54d7b6e
Brenda Mejia <mejiab19@gmail.com>
2022-12-22 16:37:05 -0600

    Created several database models and added data

--- sciencenerds/accounts/models.py
+++ sciencenerds/accounts/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Clothing and Accessories','Clothing and Accessories'),
+        ('Drinkware','Drinkware'),
+    )
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    # This variable will be set as the value for choices in the 
+    # attribute for status
+
+    # The first element in each tuple is the actual value to be set on the model, 
+    # and the second element is the human-readable name (This comment is taken from the Djanog documentation)
+
+    STATUS = (
+        ('Pending','Pending'),
+        ('Out for delivery','Out for delivery'),
+        ('Delivered','Delivered'),
+    )
+
+    # Making connection to Customer Table; This is a one to many relationship
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    # Making connection to Product Table; This is a many to many relationship
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+    
