d600de3bdd52a7e74d6b99b6615ff22bb966eef8
Brenda Mejia <mejiab19@gmail.com>
2022-12-22 18:13:19 -0600

    retrieved customer information from database, display on dashboard

--- sciencenerds/accounts/views.py
+++ sciencenerds/accounts/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import Customer, Product,Order
 class HomeView(View):
     def get(self,request):
+        orders = Order.objects.all()
+        customers = Customer.objects.all()
+
         return render(
             request = request,
             template_name= 'accounts/dashboard.html',
+            context={
+                'orders':orders, 
+                'customers':customers,
+                }
         )
 
 class ProductsView(View):
      def get(self,request):
+        products = Product.objects.all()
         return render(
             request = request,
             template_name= 'accounts/products.html',
+            context={'products':products},
         )
     
 
