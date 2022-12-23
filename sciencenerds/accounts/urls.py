f96496f60ce6a50d578cf82f05f2ae55f372c73b
Brenda Mejia <mejiab19@gmail.com>
2022-12-23 17:56:46 -0600

    create UpdateOrderView

--- sciencenerds/accounts/urls.py
+++ sciencenerds/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomeView, ProductsView, CustomerView, CreateOrderView
+from .views import HomeView, ProductsView, CustomerView, CreateOrderView, UpdateOrderView
 
 urlpatterns = [
     path('',HomeView.as_view(), name='home'),
     path('products', ProductsView.as_view(), name='products'),
     path('customer/<str:id>', CustomerView.as_view(), name='customer'),
     path('create_order',CreateOrderView.as_view(), name='create_order'),
+    path('update_order/<str:id>', UpdateOrderView.as_view(), name='update_order'),
 ]
